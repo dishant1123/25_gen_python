4d90739bca674d8737f14877a5bdd154824d040e
dishant1123 <dishantshah1417@gmail.com>
2025-10-07 18:57:01 +0530

    conditional statement

---
+++ 7oct.py
@@ -0,0 +1,143 @@
+# conditional  statement : 
+"""
+if else  : 
+
+syntax : 
+
+if con : 
+    print()
+else :
+    print()
+"""
+
+"""a=int(input("enter  the  number 1:"))
+b=int(input("enter  the  number 2:"))
+
+if a>b : 
+    print("a is big")
+else :
+    print("b is big")
+"""    
+
+# ladder  if : 
+"""
+if con : 
+    print()
+elif con : 
+    print()
+else :
+    print()
+"""
+    
+    
+"""
+a=int(input("enter  the  number 1:"))
+b=int(input("enter  the  number 2:"))
+c=int(input("enter  the  number 3:"))
+
+if a>b and a>c :
+    print("a is big")
+elif b>a and  b>c :
+    print("b is big")
+elif c>a and c>b :
+    print("c is big")
+else :
+    print("same")
+
+"""
+
+# nested  if : 
+"""
+if con : 
+    if con  :
+        print()
+    else :
+        print()
+elif con : 
+    if con :
+        print()
+    else :
+        print()
+else : 
+    print()
+"""
+
+"""a=int(input("enter  the  number 1:"))
+b=int(input("enter  the  number 2:"))
+c=int(input("enter  the  number 3:"))
+
+if a>b :
+    if a>c :
+        print("a is  big")
+    else :
+        print("c is big")
+elif  b>a :
+    if b>c :
+        print("b is big")
+    else :
+        print("c is big")
+else :
+    print("same")
+"""
+
+"""if 4 + 5 == 10:
+    print("TRUE")
+else:
+    print("FALSE")
+print("TRUE")
+
+"""
+"""x = 3
+if (x > 2):
+    x = x * 2
+if (x > 4):
+    x = 0
+print(x)
+"""
+"""x = 0
+a = 0
+b = -5
+if a > 0:
+    if b < 0: 
+        x = x + 5 
+    elif a > 5:
+        x = x + 4
+    else:
+        x = x + 3
+else:
+    x = x + 4
+print(x)
+"""
+"""a=0
+b=6
+c=9
+d=10
+x=(a and b) and ((a or c) or (b and d)) 
+print(x) 
+"""
+# 6   and  ==> nani value   or  moti value 
+#         
+# and ((a or c) or (b and d))
+# mok   mi  
+# het  darsh   pinal 0  dhruvi  dhruv  raja  maru  
+# chirag   fenil   vishwesh 
+# jay marmik 
+# krishiv  prey  preet saumya 
+ 
+"""
+task  : 1 
+
+ask user  to enter the  three number  and  print  small  medium and  large number. 
+input  :a 10   b: 20  c :30  a is  small  b is medium  c is large
+
+task :2 
+
+ask user to enter the  character and  convert in to  upper case to lower  case and  vice versa. 
+
+input  : W 
+output  :w  
+
+"""
+
+# What will be the output of this program?
+print(int("6" == 6.0) * 3 + 4 % 5)  # 4 
