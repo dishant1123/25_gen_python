05d9498795203c183d471bf38b31f82d41da803e
dishant1123 <dishantshah1417@gmail.com>
2025-10-16 15:26:18 +0530

    ...

---
+++ viral_backup.py
@@ -0,0 +1,35 @@
+# loop  : 
+"""
+1. for 
+2. while 
+3. while true 
+
+break  contunie  pass 
+"""
+# syntax : 
+"""
+for  variable name in range(start ,stop,step):
+    print(variable name)
+"""
+
+# 1-100 : 
+"""
+for i in range(1,101):
+    print(i,end=" ") 
+"""
+# 100-1 : 
+"""for x in range(100,0,-1):  # start 100 , stop 0 ,step -1
+    print(x,end=" ")
+"""
+# odd :
+"""
+for v in range(1,101,2):
+    print(v,end=" ")
+"""
+
+# even : 
+"""for v in range(0,101,2):
+    print(v,end=" ")
+
+"""
+#-90 to  45 print  
