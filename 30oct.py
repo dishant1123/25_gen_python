a9ffc4fcb547a11280898e6163ddf886ab55de2a
dishant1123 <dishantshah1417@gmail.com>
2025-10-30 18:49:00 +0530

    ....

--- 30oct.py
+++ 30oct.py
@@ -41,7 +41,7 @@ if sum ==temp:
 """
 # strong  number in range : 
 
-start = int(input("enter the  start number  : "))
+"""start = int(input("enter the  start number  : "))
 end=int(input("enter the  end number  : "))
 
 for i in range(start, end+1):  # 101   100000 
@@ -57,5 +57,36 @@ for i in range(start, end+1):  # 101   100000
         temp =  temp //10 
     if sum ==i : 
         print(i,end=" ") 
-
+"""
 # pelindrome   in range  :    ==> 100  100000 
+
+# pattern  : 
+""" 
+1.         2.         3.          4.          5.           6.         7. 
+* * * * *  *          * * * * *   1            5 4 3 2 1   1 2 3 4 5  a 
+* * * * *  * *        * * * *     1 2          5 4 3 2     2 3 4 5    a b 
+* * * * *  * * *      * * *       1 2 3        5 4 3       3 4 5      a b c 
+* * * * *  * * * *    * *         1 2 3 4      5 4         4 5        a b c d
+* * * * *  * * * * *  *           1 2 3 4 5    5           5          a b c d e
+
+"""
+#1 : 
+"""
+for i in range(1,6):  # 1 
+    for j in range(1,6):  # 1 
+        print("*",end=" ")
+    print()
+"""
+#2 : 
+"""
+for i in range(1,6):  # 1 
+    for j in range(1,i+1):  # 1 ,1  
+        print("*",end=" ")
+    print()
+"""
+#3 : 
+
+for i in range(1,6):  # 1 
+    for j in range(5,i-1,-1):  # 1 ,1  
+        print("*",end=" ")
+    print()
