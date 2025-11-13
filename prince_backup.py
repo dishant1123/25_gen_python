8b8e96e0c69bb0670d1e28c0f5f866f0f6afec31
dishant1123 <dishantshah1417@gmail.com>
2025-11-13 13:45:03 +0530

    ...

--- prince_backup.py
+++ prince_backup.py
@@ -198,108 +198,226 @@ for i in range(1,6):   # 1
 
 #1. string  2 list  3 tuple 4 dict  5 set 
 """
-#list : mutable  sequence element  ==> you can changes in list. 
+# #list : mutable  sequence element  ==> you can changes in list. 
+
+# """
+# l1=[1,2,3,4,56,7,8,9,"jay",9j,90.89]
+# print(l1)
+# print(type(l1))
+ 
+# """
+# # list element access though  index : 
+# """
+# l1=[1,2,3,4,56,7,8,9,"jay",9j,90.89]
+
+# print(l1[0])
+# l1[2] ="viral"
+# print(l1)
+# """
+# # slicing  : 
+
+# """
+# l1=[100,2,3,4,56,7,8,9,90,91,90]
+# # pos index : l to  r  ==> 0 
+# # neg index : r to l  ==>-1 
+
+# print(l1[4])
+# print(l1[2:4])
+# print(l1[2:4])
+# print(l1[ :8])
+# print(l1[1 : ])
+# print(l1[-1])
+# print(l1[-5 : -1])
+# print(l1[5 : -1])
+# print(l1[-5 : 1])
+# #       -5 -4-3-2-1 0 1 2 3 4 5
+
+# print(l1[2: 8 :2])
+# print(l1[1: 8 :3])
+# print(l1[ :   :2])
+# print(l1[ :   :-2])
+# print(l1[ :   :-1])
+
+# """
+
+# # built in function  : len min max sorted sum 
+# """
+# l1=[100,2,3,4,56,7,8,9,90,91,90]
+# print(len(l1))
+# print(min(l1))
+# print(max(l1))
+# print(sorted(l1))   # asc to desc
+# print(sorted(l1,reverse=True))
+# print(sum(l1))
+# """
+
+# # method  : 
+# l1=[100,2,3,4,56,7,8,9,90,91,90]
+
+# """
+# l1.append(2000)  # add element in last 
+# print(l1)
+
+# l2= l1.copy()
+# print("l2=",l2)
+
+# """
+# """
+# l1.clear()
+# print(l1)
+# """
+
+# """
+# l2=["apple","banana","cherry"]
+
+# l1.extend(l2)
+# print(l1)
+# """
+
+# # index : 
+# """
+# l1=[100,2,3,4,56,7,8,9,90,91,90,56]
+
+# print(l1.index(56))
+# print(l1.index(56,5,20))
+
+# """
+
+# l1=[100,2,3,4,56,7,8,9,90,91,90,56]
+
+# # remove  , pop : 
+# # l1.pop(4)  # arg  ==> index 
+# # print(l1)
+
+# """
+# l1.remove(8)
+# print(l1)
+# """
+# # l1.sort()
+# # print(l1)
+
+# """
+# l1.reverse()
+# print(l1)
+# """
+
+# print(l1.count(56))
+# """
+
+# data type  : list , tuple , dict, set, string 
+
+#list : mutable sequence element ==> you can changes in list.
 
 """
 l1=[1,2,3,4,56,7,8,9,"jay",9j,90.89]
+
 print(l1)
 print(type(l1))
- 
+
 """
-# list element access though  index : 
+
+# list element access though  index :
+
 """
 l1=[1,2,3,4,56,7,8,9,"jay",9j,90.89]
-
 print(l1[0])
-l1[2] ="viral"
+l1[3] ="prince"  # update element  ==>though index 
 print(l1)
 """
-# slicing  : 
+
+# built in function  : len  min max sorted sum 
 
 """
-l1=[100,2,3,4,56,7,8,9,90,91,90]
+list1=[1,2,3,4,56,7,8,9,56,9,90.89]
+
+print(len(list1))
+print(min(list1))
+print(max(list1))
+print(sorted(list1))  # asc to desc
+print(sorted(list1,reverse=True)) # desc to asc 
+print(sum(list1))
+"""
+
+# slicing : 
+"""l1=[1,2,3,4,56,7,8,9,56,9,90.89]
 # pos index : l to  r  ==> 0 
-# neg index : r to l  ==>-1 
+# neg index : r to l  ==>-1
 
 print(l1[4])
-print(l1[2:4])
-print(l1[2:4])
-print(l1[ :8])
-print(l1[1 : ])
-print(l1[-1])
-print(l1[-5 : -1])
-print(l1[5 : -1])
-print(l1[-5 : 1])
-#       -5 -4-3-2-1 0 1 2 3 4 5
+print(l1[2 :5])   # start 2  end 5  ==>ending index is not included
+print(l1[: 6])
+print(l1[1: ])
+print(l1[-2])
 
-print(l1[2: 8 :2])
-print(l1[1: 8 :3])
-print(l1[ :   :2])
-print(l1[ :   :-2])
-print(l1[ :   :-1])
+print(l1[2 :8:2]) # start 2  end 8 ==>step 2
+print(l1[1 :8: 3]) # start 1  end 8 ==>step 3
 
-"""
-
-# built in function  : len min max sorted sum 
-"""
-l1=[100,2,3,4,56,7,8,9,90,91,90]
-print(len(l1))
-print(min(l1))
-print(max(l1))
-print(sorted(l1))   # asc to desc
-print(sorted(l1,reverse=True))
-print(sum(l1))
+print(l1[ : : 2])
+print(l1[ : : -2])
+print(l1[ : : -1])
 """
 
 # method  : 
-l1=[100,2,3,4,56,7,8,9,90,91,90]
 
-"""
-l1.append(2000)  # add element in last 
-print(l1)
+l1=[1,2,3,4,56,7,8,9,56,9,90]
 
-l2= l1.copy()
-print("l2=",l2)
+# l1.append(2000) 
+# print(l1)
 
-"""
-"""
-l1.clear()
-print(l1)
-"""
+# l1.insert(2,234)
+# print(l1)
+
+# l1.clear()
+# print(l1)
+
+# l2 =l1.copy()
+# print(l2)
+
+# print(l1.count(56))
+
+l1=[1,2,3,4,56,7,8,9,56,9,90]
+
+# print(l1.index(56))
+# print(l1.index(56,5,20))
 
 """
 l2=["apple","banana","cherry"]
-
 l1.extend(l2)
 print(l1)
 """
 
-# index : 
-"""
-l1=[100,2,3,4,56,7,8,9,90,91,90,56]
-
-print(l1.index(56))
-print(l1.index(56,5,20))
+# delete  :  pop () , remove()
 
 """
-
-l1=[100,2,3,4,56,7,8,9,90,91,90,56]
-
-# remove  , pop : 
-# l1.pop(4)  # arg  ==> index 
-# print(l1)
-
-"""
-l1.remove(8)
+l1.pop(5)  # arg :index
 print(l1)
-"""
-# l1.sort()
-# print(l1)
 
-"""
-l1.reverse()
+l1.remove(56)
 print(l1)
 """
 
-print(l1.count(56))
+"""l1.sort()
+print(l1)
 """
+
+"""l1.reverse()
+print(l1)
+"""
+
+# task :1 
+
+n=int(input("enter the  number :"))
+l2=[]
+for i in range(1,n+1):
+    element =int(input("enter the  number :"))
+    l2.append(element)
+print(l2)  # [1,2,3,4,5]
+even=0
+odd=0
+for i in l2 :
+    if i % 2==0:
+        even=even+i 
+    else :
+        odd=odd+i
+print(even)
+print(odd)
