34c0e413116591bdabcd1dfe14b3c48481cac6a4
dishant1123 <dishantshah1417@gmail.com>
2025-12-23 18:28:18 +0530

    ....

---
+++ 23dec.py
@@ -0,0 +1,57 @@
+# string  : immutable sequence of characters
+
+"""
+s1="my name is ram."
+print(s1)
+print(type(s1))
+"""
+
+# built in function  : len min max sorted sum 
+"""s1="my name is ram."
+
+print(len(s1))
+print(min(s1)) # ascci value 
+print(max(s1))
+print(sorted(s1))
+print(sorted(s1,reverse=True))
+"""
+# concatenate two string :
+
+"""a="pinal"
+b="patel"
+print(a+b)
+"""
+
+# slicing  : 
+
+"""s1="my name is pinal patel and live in ahmedabad."
+#index start  with  : 0   ==> l to r 
+#neg index start  with  : -1 ==> r to l 
+
+print(s1[2])
+print(s1[3:9])
+print(s1[-2])
+print(s1[-5 : -1])
+print(s1[-1 : -5])  #      -5-4-3-2-1  0
+print(s1[-1 : -5 : -1])  #
+print(s1[2: 7 :2])
+print(s1[:  :2])
+print(s1[:  :-2])
+print(s1[:  :-1])
+"""
+
+# task  :1 
+"""
+input  : "dishant dipakkuamr shah"
+output  : d.d.shah
+"""
+# task  :2 
+"""
+ask user to enter the two string and  swap the first three character of the second string and wise versa.
+
+input  a : color 
+input  b : full 
+
+output a: fulor 
+output b: coll  
+"""
