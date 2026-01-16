e8f014197df53e04835490153756888c0ef10de7
dishant1123 <dishantshah1417@gmail.com>
2026-01-16 19:25:04 +0530

    .....

---
+++ 16jan.py
@@ -0,0 +1,44 @@
+# lambda function  : 
+"""
+one  line  function  . 
+
+syntax : lambda args : expression 
+"""
+
+# ex : 1 
+
+"""
+def add(x,y):
+    return x+y 
+print(add(1,67))
+"""
+"""x= lambda a,b :a+b
+print(x(23,56))
+"""
+# ex 2 :  len min max sorted  
+
+"""a=lambda x :sorted(x) 
+print(a((1,2,30,4,7,5)))
+"""
+
+# ex :3 using conditional statements
+"""
+def big(a,b):
+    if a>b :
+        print("a is bigger")
+    else :
+        print("b is bigger")
+big(10,20)
+"""
+"""
+x =lambda a,b : print("a is  big")if a>b else print("b is big")
+x(90,3) 
+b= lambda a,b,c : max(a,b,c)
+print(b(10,20,30))
+"""
+
+# task  :1 sort by second element. (using def function ) 
+"""
+input  : a= [[1,2],[0,4],[5,-2]]
+output  : a= [[5,-2],[1,2],[0,4]]
+"""
