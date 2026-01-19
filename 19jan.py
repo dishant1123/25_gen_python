a6272e36d95e4810507b7cfe08ccc36965b4a4ff
dishant1123 <dishantshah1417@gmail.com>
2026-01-19 20:26:37 +0530

    ,

--- 19jan.py
+++ 19jan.py
@@ -55,4 +55,12 @@ Original lists:
 Reverse strings of the said given list:
 ['deR', 'neerG', 'eulB', 'etihW', 'kcalB']
 
+
+6. 
+Write a Python program to find numbers divisible by nineteen or thirteen from a list of numbers using Lambda.
+
+Orginal list:
+[19, 65, 57, 39, 152, 639, 121, 44, 90, 190]
+Numbers of the above list divisible by nineteen or thirteen:
+[19, 65, 57, 39, 152, 190]
 """
