8ac13541f441698a5f8eff05ebb6849acc11f487
dishant1123 <dishantshah1417@gmail.com>
2026-02-03 18:14:32 +0530

    ....

---
+++ 3feb.py
@@ -0,0 +1,18 @@
+# number  guessing game  : 
+"""
+number  ==> computer  guess ==> range   ==> 1,20   ==> guess = 15 
+             
+user total attempt =4 
+1 attempt :6   2 attempt : 14   3 attempt : 15 
+congrats   !!!  you win 
+
+logic :  
+1. loop        
+2. if (con) : user_num > guess_num   : too  high 
+3. if (con) : user_num < guess_num   : too low 
+
+else :  you win  
+
+print("u  loss",computer_guess) 
+
+"""
