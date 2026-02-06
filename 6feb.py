204ad98f34720a6cab78c2873cc182d37ad6031e
dishant1123 <dishantshah1417@gmail.com>
2026-02-06 19:18:41 +0530

    .....

---
+++ 6feb.py
@@ -0,0 +1,9 @@
+from mypackage2 import function as f ,moksh as m 
+
+print(f.add(90,12))
+print(f.sub(90,12))
+print(f.mul(90,12))
+print(f.div(90,12))
+print(f.mod(90,12))
+
+m.m_patel()
---
+++ mypackage2/moksh.py
@@ -0,0 +1,4 @@
+def m_patel():
+    print("hello  moksh  !!!! how  are  you  ??? ")
+    
+    
