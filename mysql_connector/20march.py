fdc4da173a02cc009e2bfdf13236692b2ee7b679
dishant1123 <dishantshah1417@gmail.com>
2026-03-24 18:20:45 +0530

    ....

--- mysql_connector/20march.py
+++ mysql_connector/20march.py
@@ -6,15 +6,38 @@ mydb = mysql.connector.connect(
     host="localhost",
     port=3306,
     user="root",
-    password="root"
+    password="root",
+    database="25_gen_python"
 )
 
 conn =mydb.cursor()
 
 # create database :   ==> create database  database_name; 
 
-conn.execute("CREATE DATABASE 25_gen_python") 
-print("Database created successfully")
+# conn.execute("CREATE DATABASE 25_gen_python") 
+# print("Database created successfully")
 
+'''conn.execute("""
+             
+             CREATE TABLE IF NOT EXISTS student (
+                 id INT AUTO_INCREMENT PRIMARY KEY,
+                 name VARCHAR(50),
+                 salary INT
+             )
+             """)
+'''
+# print("TABLE created successfully")
+
+conn.execute("INSERT INTO student (name, salary) VALUES ('moksh', 100000)")
+conn.execute("INSERT INTO student (name, salary) VALUES ('pinal', 200000)")
+conn.execute("INSERT INTO student (name, salary) VALUES ('dhruv', 90000)")
+conn.execute("INSERT INTO student (name, salary) VALUES ('keshav', 80000)")
+conn.execute("INSERT INTO student (name, salary) VALUES ('het', 10000)")
+
+mydb.commit()
+print("Data inserted successfully")
 conn.close()
 mydb.close()
+
+# update  : moksh  het  salary 
+# delete  : keshav 
